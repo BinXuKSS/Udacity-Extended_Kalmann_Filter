c8186228b9a3ea72e1cf203125955f60639bdcb2
BinXu <15962108591@163.com>
2017-11-11 22:28:14 +0800

    update

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -87,6 +87,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       ekf_.x_(0) = measurement_pack.raw_measurements_(0)*cos(measurement_pack.raw_measurements_(1));
 	  ekf_.x_(1) = measurement_pack.raw_measurements_(0)*sin(measurement_pack.raw_measurements_(1));
+      ekf_.x_(2) = measurement_pack.raw_measurements_(2)*cos(measurement_pack.raw_measurements_(1));
+	  ekf_.x_(3) = measurement_pack.raw_measurements_(2)*sin(measurement_pack.raw_measurements_(1));
+
+	  
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -94,6 +98,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       ekf_.x_(0) = measurement_pack.raw_measurements_(0);
 	  ekf_.x_(1) = measurement_pack.raw_measurements_(1);
+	  ekf_.x_(2) = 0;
+	  ekf_.x_(3) = 0;
     }
 
 	previous_timestamp_ = measurement_pack.timestamp_;
